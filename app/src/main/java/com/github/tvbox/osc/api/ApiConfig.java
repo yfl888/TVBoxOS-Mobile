9501eba23d76d520c6521b92b3bd6420ef139d7b
yfl888 <50236927+yfl888@users.noreply.github.com>
2024-02-25 18:27:29 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -128,7 +128,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://d.kstore.space/download/6656/db.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
